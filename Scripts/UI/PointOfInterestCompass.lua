0e05a53a0bfbea9af5c2d2624baec3fa15d15b0f
Mercor <mercor.eats.waffles@gmail.com>
2015-04-23 14:17:18 +0300

    Compass: Minor reorganisation of bloated methods + addition of input mapping context

--- Scripts/UI/PointOfInterestCompass.lua
+++ Scripts/UI/PointOfInterestCompass.lua
@@ -24,115 +24,13 @@ function PointOfInterestCompass:PostLoad(args)
     {name = "triforce", 	imageName = "PoI-Icons/triforce"},
     {name = "sheep", 			imageName = "PoI-Icons/sheep"}
   }
-
   self.m_default_type_image = "PoI-Icons/forest"
 
-  self.m_compass = self:GetChild("Compass")
-  self.m_north = self:GetChild("Compass/North")
-  self.m_east = self:GetChild("Compass/East")
-  self.m_south = self:GetChild("Compass/South")
-  self.m_west = self:GetChild("Compass/West")
-  self.m_options_button = self:GetChild("Compass/PoI Button")
-  self.m_radar = self:GetChild("Compass/Radar")
+  self:CreateInputMappingContext()
 
-  -- target poi label
-  self.m_target_poi_label = self:GetChild("Compass/PoI Label")
-  self.m_target_poi_label:setProperty("Visible", "false")
-  self.m_target_poi_label_visible = false
-  self.m_poi_id_in_target_poi_label = nil
+  self:InitElements()
 
-  -- location scroll
-  self.m_location_scroll = self:GetChild("Location Scroll")
-  self.m_location_scroll:setProperty("Visible", "false")
-  self.m_location_scroll_visible = false
-  self.m_poi_id_in_location_scroll = nil
-
-  self.m_location_scroll_text = self:GetChild("Location Scroll/Label")
-  self.m_location_scroll_text:setText("")
-
-  -- options
-  self.m_options_window = self:GetChild("Options Window")
-  self.m_options_window:setProperty("Visible", "false")
-  self.m_options_window_visible = false
-
-
-  -- options > create
-  self.m_create_poi_button = self:GetChild("Options Window/Options Container/Create PoI Button")
-  self.m_close_options_window_button = self:GetChild("Options Window/Header/Close Button")
-
-  -- options > create > fields
-  self.m_title_field = self:GetChild("Options Window/Options Container/Title Field/Editbox")
-  self.m_type_field = self:GetChild("Options Window/Options Container/Type Field/Editbox")
-  self.m_new_poi_type = nil
-  self.m_description_field = self:GetChild("Options Window/Options Container/Description Field/Editbox")
-
-  -- options > create > type buttons
-  self.m_type_sheep_button = self:GetChild("Options Window/Options Container/Icon Picker/Sheep Button")
-  self.m_type_triforce_button = self:GetChild("Options Window/Options Container/Icon Picker/Triforce Button")
-  self.m_type_panda_button = self:GetChild("Options Window/Options Container/Icon Picker/Panda Button")
-  self.m_type_lighthouse_button = self:GetChild("Options Window/Options Container/Icon Picker/Lighthouse Button")
-  self.m_type_forest_button = self:GetChild("Options Window/Options Container/Icon Picker/Forest Button")
-
-  -- options > list pois
-  self.m_list_tab = self:GetChild("List PoIs Window")
-  self.m_list_tab:setProperty("Visible", "false")
-
-  -- radar items
-  self.m_items = {}
-  self.m_pois = {}
-
-  -- TODO: for each poi in poiMain add item to self.m_items
-  -- TODO: register to poiMain, so that every time new poi is added, it's added to radar
-
-  self.m_north:setPosition(CEGUI.UVector2(CEGUI.UDim(0, 0), CEGUI.UDim(0, 0)))
-
-  -- debug window
-  -- self.m_text = Windows:createWindow("TUGLook/StaticText")
-  -- self.m_containerWindow:addChild(self.m_text)
-  -- self.m_text:setArea(CEGUI.UDim(0.04, 0), CEGUI.UDim(0.04, 0), CEGUI.UDim(0.24, 0), CEGUI.UDim(0.24, 0))
-  -- self.m_text:setHeight(CEGUI.UDim(1, 0))
-  -- self.m_text:setProperty("HorzFormatting", "WordWrapLeftAligned")
-  -- self.m_text:setProperty("VertFormatting", "TopAligned")
-  -- self.m_text:setProperty("BackgroundEnabled", "false")
-  -- self.m_text:setProperty("FrameEnabled", "false")
-  --
-  -- self.m_text:setText("testing")
-
-	self.m_options_button:subscribeEvent("MouseClick", function( args )
-    self:Debug("PointOfInterestMod's options button was clicked")
-    if EternusEngine.mods.PointOfInterest.ConsoleUI then
-		  EternusEngine.mods.PointOfInterest.ConsoleUI:WriteMessageToChat("Point Of Interest options btn clicked.")
-    end
-
-    if self.m_options_window_visible then
-      self:HideOptionsWindow()
-    else
-      self:ShowOptionsWindow()
-    end
-  end)
-
-  self.m_close_options_window_button:subscribeEvent("MouseClick", function( args )
-    self:Debug("PointOfInterestMod's close options button was clicked")
-    if EternusEngine.mods.PointOfInterest.ConsoleUI then
-      EternusEngine.mods.PointOfInterest.ConsoleUI:WriteMessageToChat("Point Of Interest options's x btn clicked.")
-    end
-    self:HideOptionsWindow()
-  end)
-
-  self.m_create_poi_button:subscribeEvent("MouseClick", function( args )
-    self:Debug("PointOfInterestMod's create poi button was clicked")
-    if EternusEngine.mods.PointOfInterest.ConsoleUI then
-      EternusEngine.mods.PointOfInterest.ConsoleUI:WriteMessageToChat("Point Of Interest created.")
-    end
-    self:CreatePointOfInterest()
-    self:HideOptionsWindow()
-  end)
-
-  self.m_type_sheep_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("sheep") end) -- Sheep
-  self.m_type_panda_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("panda") end) -- danger
-  self.m_type_triforce_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("triforce") end) -- Spiritual location
-  self.m_type_forest_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("forest") end) -- Landmark
-  self.m_type_lighthouse_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("lighthouse") end) -- Building
+  self:RegisterEvents()
 end
 
 -------------------------------------------------------------------------------
@@ -213,6 +111,120 @@ function PointOfInterestCompass:Update( dt )
   else
     self:HideTargetPoILabel()
   end
+end
+
+
+function PointOfInterestCompass:InitElements()
+  self.m_compass = self:GetChild("Compass")
+  self.m_north = self:GetChild("Compass/North")
+  self.m_east = self:GetChild("Compass/East")
+  self.m_south = self:GetChild("Compass/South")
+  self.m_west = self:GetChild("Compass/West")
+  self.m_options_button = self:GetChild("Compass/PoI Button")
+  self.m_radar = self:GetChild("Compass/Radar")
+
+  -- target poi label
+  self.m_target_poi_label = self:GetChild("Compass/PoI Label")
+  self.m_target_poi_label:setProperty("Visible", "false")
+  self.m_target_poi_label_visible = false
+  self.m_poi_id_in_target_poi_label = nil
+
+  -- location scroll
+  self.m_location_scroll = self:GetChild("Location Scroll")
+  self.m_location_scroll:setProperty("Visible", "false")
+  self.m_location_scroll_visible = false
+  self.m_poi_id_in_location_scroll = nil
+
+  self.m_location_scroll_text = self:GetChild("Location Scroll/Label")
+  self.m_location_scroll_text:setText("")
+
+  -- options
+  self.m_options_window = self:GetChild("Options Window")
+  self.m_options_window:setProperty("Visible", "false")
+  self.m_options_window_visible = false
+
+
+  -- options > create
+  self.m_create_poi_button = self:GetChild("Options Window/Options Container/Create PoI Button")
+  self.m_close_options_window_button = self:GetChild("Options Window/Header/Close Button")
+
+  -- options > create > fields
+  self.m_title_field = self:GetChild("Options Window/Options Container/Title Field/Editbox")
+  self.m_type_field = self:GetChild("Options Window/Options Container/Type Field/Editbox")
+  self.m_new_poi_type = nil
+  self.m_description_field = self:GetChild("Options Window/Options Container/Description Field/Editbox")
+
+  -- options > create > type buttons
+  self.m_type_sheep_button = self:GetChild("Options Window/Options Container/Icon Picker/Sheep Button")
+  self.m_type_triforce_button = self:GetChild("Options Window/Options Container/Icon Picker/Triforce Button")
+  self.m_type_panda_button = self:GetChild("Options Window/Options Container/Icon Picker/Panda Button")
+  self.m_type_lighthouse_button = self:GetChild("Options Window/Options Container/Icon Picker/Lighthouse Button")
+  self.m_type_forest_button = self:GetChild("Options Window/Options Container/Icon Picker/Forest Button")
+
+  -- options > list pois
+  self.m_list_tab = self:GetChild("List PoIs Window")
+  self.m_list_tab:setProperty("Visible", "false")
+
+  -- radar items
+  self.m_items = {}
+  self.m_pois = {}
+
+  -- TODO: for each poi in poiMain add item to self.m_items
+  -- TODO: register to poiMain, so that every time new poi is added, it's added to radar
+
+  self.m_north:setPosition(CEGUI.UVector2(CEGUI.UDim(0, 0), CEGUI.UDim(0, 0)))
+
+  -- debug window
+  -- self.m_text = Windows:createWindow("TUGLook/StaticText")
+  -- self.m_containerWindow:addChild(self.m_text)
+  -- self.m_text:setArea(CEGUI.UDim(0.04, 0), CEGUI.UDim(0.04, 0), CEGUI.UDim(0.24, 0), CEGUI.UDim(0.24, 0))
+  -- self.m_text:setHeight(CEGUI.UDim(1, 0))
+  -- self.m_text:setProperty("HorzFormatting", "WordWrapLeftAligned")
+  -- self.m_text:setProperty("VertFormatting", "TopAligned")
+  -- self.m_text:setProperty("BackgroundEnabled", "false")
+  -- self.m_text:setProperty("FrameEnabled", "false")
+  --
+  -- self.m_text:setText("testing")
+
+end
+
+function PointOfInterestCompass:RegisterEvents()
+  self.m_options_button:subscribeEvent("MouseClick", function( args )
+    self:Debug("PointOfInterestMod's options button was clicked")
+    if EternusEngine.mods.PointOfInterest.ConsoleUI then
+		  EternusEngine.mods.PointOfInterest.ConsoleUI:WriteMessageToChat("Point Of Interest options btn clicked.")
+    end
+
+    if self.m_options_window_visible then
+      self:HideOptionsWindow()
+    else
+      self:ShowOptionsWindow()
+    end
+  end)
+
+  self.m_close_options_window_button:subscribeEvent("MouseClick", function( args )
+    self:Debug("PointOfInterestMod's close options button was clicked")
+    if EternusEngine.mods.PointOfInterest.ConsoleUI then
+      EternusEngine.mods.PointOfInterest.ConsoleUI:WriteMessageToChat("Point Of Interest options's x btn clicked.")
+    end
+    self:HideOptionsWindow()
+  end)
+
+  self.m_create_poi_button:subscribeEvent("MouseClick", function( args )
+    self:Debug("PointOfInterestMod's create poi button was clicked")
+    if EternusEngine.mods.PointOfInterest.ConsoleUI then
+      EternusEngine.mods.PointOfInterest.ConsoleUI:WriteMessageToChat("Point Of Interest created.")
+    end
+    self:CreatePointOfInterest()
+    self:HideOptionsWindow()
+  end)
+
+  self.m_type_sheep_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("sheep") end) -- Sheep
+  self.m_type_panda_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("panda") end) -- danger
+  self.m_type_triforce_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("triforce") end) -- Spiritual location
+  self.m_type_forest_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("forest") end) -- Landmark
+  self.m_type_lighthouse_button:subscribeEvent("MouseClick", function( args ) self:SetNewPoIType("lighthouse") end) -- Building
+
 end
 
 -------------------------------------------------------------------------------
@@ -384,6 +396,8 @@ end
 function PointOfInterestCompass:ShowOptionsWindow()
   self.m_options_window_visible = true
   self.m_options_window:setProperty("Visible", "true")
+
+  self:GrabInputMappingContext()
 end
 
 -------------------------------------------------------------------------------
@@ -391,6 +405,24 @@ end
 function PointOfInterestCompass:HideOptionsWindow()
   self.m_options_window:setProperty("Visible", "false")
   self.m_options_window_visible = false
+
+  self:ReleaseInputMappingContext()
+end
+
+function PointOfInterestCompass:CreateInputMappingContext()
+  self.m_optionsContext = InputMappingContext.new("PointOfInterest > Options")
+
+  self.m_optionsContext:NKSetInputPropagation(false)
+
+  -- define esc as escape key
+end
+
+function PointOfInterestCompass:GrabInputMappingContext()
+  Eternus.InputSystem:NKPushInputContext(self.m_optionsContext)
+end
+
+function PointOfInterestCompass:ReleaseInputMappingContext()
+  Eternus.InputSystem:NKRemoveInputContext(self.m_optionsContext)
 end
 
 function PointOfInterestCompass:Debug(msg)
